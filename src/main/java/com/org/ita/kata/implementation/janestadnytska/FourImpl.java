bad6e03c002f2d98a3d6ce0f05948775700a32b5
Zhanna Stadnytska <janelischuk@gmail.com>
2023-01-24 23:50:50 +0200

    four/janestadnytska

--- src/main/java/com/org/ita/kata/implementation/janestadnytska/FourImpl.java
+++ src/main/java/com/org/ita/kata/implementation/janestadnytska/FourImpl.java
@@ -3,4 +3,7 @@ package com.org.ita.kata.implementation.janestadnytska;
 import com.org.ita.kata.Four;
 
 public class FourImpl implements Four {
+public String warrior (String str) {
+    return "The Greatest Warrior task is not done yet.";
+}
 }
