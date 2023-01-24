e3ca028191e7d82b5df1796d36d57436c7987575
Denis Pitsul <denia.pitsul@gmail.com>
2023-01-24 17:37:29 +0200

    Added solutions to 8 kata

--- src/main/java/com/org/ita/kata/implementation/DenisPitsul/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DenisPitsul/EightImpl.java
@@ -2,44 +2,65 @@ package com.org.ita.kata.implementation.DenisPitsul;
 
 import com.org.ita.kata.Eight;
 
+import java.util.Arrays;
+
 public class EightImpl implements Eight {
     @Override
     public int Liters(double time) {
-        return 0;
+        return (int) (time * 0.5);
     }
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return 0;
+        return length * width * height;
     }
 
     @Override
     public float mpgToKPM(float mpg) {
-        return 0;
+        double kpm = (float) (mpg * 1.609344) / 4.54609188;
+        return Math.round(kpm * 100.0) / 100.0f;
     }
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        int[] newArray = new int[array.length];
+        for(int i = 0; i < array.length; i++) {
+            int number = (int) Math.sqrt(array[i]);
+            if(Math.pow(number, 2) == array[i])
+                newArray[i] = (int) Math.sqrt(array[i]);
+            else
+                newArray[i] = (int) Math.pow(array[i], 2);
+        }
+        return newArray;
     }
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        if (input == null || input.length == 0)
+            return new int[] {};
+
+        int[] newArray = new int[2];
+        for(int i = 0; i < input.length; i++) {
+            if(input[i] > 0)
+                newArray[0]++;
+            else if(input[i] < 0)
+                newArray[1] += input[i];
+        }
+        return newArray;
     }
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+        return Integer.parseInt(str);
     }
 
     @Override
     public double TwoDecimalPlaces(double number) {
-        return 0;
+        return Math.round(number * 100.0) / 100.0;
     }
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        return Arrays.stream(numbers).filter(number -> (number % divider) == 0).toArray();
     }
 }
