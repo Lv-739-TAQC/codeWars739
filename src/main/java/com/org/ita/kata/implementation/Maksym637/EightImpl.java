e85151d2b8ea48cc463d4d1208429cd9a7e417e9
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2023-01-24 19:30:51 +0200

    Adding solutions to 7-kata

--- src/main/java/com/org/ita/kata/implementation/Maksym637/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/Maksym637/EightImpl.java
@@ -2,77 +2,44 @@ package com.org.ita.kata.implementation.Maksym637;
 
 import com.org.ita.kata.Eight;
 
-import java.util.Arrays;
-
 public class EightImpl implements Eight {
     @Override
     public int Liters(double time) {
-        double liters = 0.5;
-        return (int) Math.floor(time * liters);
+        return 0;
     }
 
     @Override
     public double getVolumeOfCuboid(double length, double width, double height) {
-        return length * width * height;
+        return 0;
     }
 
     @Override
     public float mpgToKPM(float mpg) {
-        float gallon = 4.54609188f;
-        float mile = 1.609344f;
-        float result = (mpg * mile) / gallon;
-        return (float) Math.round(result * 100) / 100;
+        return 0;
     }
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        int[] newArr = new int[array.length];
-
-        for (int i = 0; i < array.length; i++) {
-            if (isSquareInteger(array[i])) {
-                newArr[i] = (int) Math.sqrt(array[i]);
-            } else {
-                newArr[i] = array[i] * array[i];
-            }
-        }
-        return newArr;
-    }
-
-    static boolean isSquareInteger(double x) {
-        double sq = Math.sqrt(x);
-        return ((sq - Math.floor(sq)) == 0);
+        return new int[0];
     }
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        int[] emptyArray = {};
-        if (input == null || input.length == 0) {
-            return emptyArray;
-        }
-        int count = 0;
-        int sum = 0;
-        for (int j : input) {
-            if (j > 0) {
-                count++;
-            } else if (j < 0) {
-                sum += j;
-            }
-        }
-        return new int[]{count, sum};
+        return new int[0];
     }
 
     @Override
     public int stringToNumber(String str) {
-        return Integer.parseInt(str);
+        return 0;
     }
 
     @Override
     public double TwoDecimalPlaces(double number) {
-        return Math.round(number * 100.0) / 100.0;
+        return 0;
     }
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return Arrays.stream(numbers).filter(number -> (number % divider) == 0).toArray();
+        return new int[0];
     }
 }
