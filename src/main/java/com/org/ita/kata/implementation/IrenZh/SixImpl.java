6a9de8ca8520be49ed0b4e7662bbb826bd2af7a4
Ira Zhurakivska <zhurakivskairen@ukr.net>
2023-01-25 21:55:24 +0200

    Impementation 3 tasks from six kata

--- src/main/java/com/org/ita/kata/implementation/IrenZh/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/IrenZh/SixImpl.java
@@ -5,17 +5,37 @@ import com.org.ita.kata.Six;
 public class SixImpl implements Six {
     @Override
     public long findNb(long m) {
-        return 0;
+        long mm = 0, n = 0;
+        while (mm < m) mm += ++n * n * n;
+        return mm == m ? n : -1;
     }
+
 
     @Override
     public String balance(String book) {
-        return null;
+        String t = book.replaceAll("([^\\n. \\da-zA-Z])", "");
+        String[] arr = t.split("[\\n]+");
+        double current = Double.parseDouble(arr[0]);
+        double total = 0;
+        int count = 0;
+        StringBuilder result = new StringBuilder();
+        result.append("Original Balance: " + arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            count++;
+            String[] line = arr[i].split("[ ]+");
+            current -= Double.parseDouble(line[2]);
+            total += Double.parseDouble(line[2]);
+            String u = String.format("\\r\\n%s %s %s Balance %.2f", line[0], line[1], line[2], current);
+            result.append(u);
+        }
+        result.append(String.format("\\r\\nTotal expense  %.2f\\r\\nAverage expense  %.2f", total, total / count));
+        return result.toString();
     }
+
 
     @Override
     public double f(double x) {
-        return 0;
+        return Math.expm1(Math.log1p(x) / 2);
     }
 
     @Override
