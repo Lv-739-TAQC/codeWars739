d64fa369d21f63f0f4287c0d6736409f2891ecb8
Dmytro Savchenko <dmitriysavchenko11@gmail.com>
2023-01-26 15:47:29 +0200

    seven/savaxn04

--- src/main/java/com/org/ita/kata/implementation/savaxn04/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/savaxn04/SevenImpl.java
@@ -2,14 +2,32 @@ package com.org.ita.kata.implementation.savaxn04;
 
 import com.org.ita.kata.Seven;
 
+import java.util.Arrays;
+
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double number = navg * (arr.length+1);
+        double sum = Arrays.stream(arr).sum();
+        if(number - sum <= 0){
+            throw new IllegalArgumentException("--- Exception 1 ---");
+        }
+        return (long)Math.ceil(number - sum);
     }
 
     @Override
     public String seriesSum(int n) {
-        return null;
+        if(n == 0){
+            return "0.00";
+        }
+        double result = 1;
+        for(int i = 1; i < n; i++){
+            result += 1.0/(3.0*i+1.0);
+        }
+        String string = String.valueOf(Math.round(result * 100.0) / 100.0);
+        if(string.length() == 3){
+            return string + "0";
+        }
+        return string;
     }
 }
