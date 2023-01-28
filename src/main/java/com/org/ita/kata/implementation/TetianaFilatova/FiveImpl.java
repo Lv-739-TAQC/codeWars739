84c37aea5fd905e524ea6acd62a3a3823b8caa0d
Tetiana Filatova <t.filatova0564@gmail.com>
2023-01-29 00:06:14 +0200

    initial commit fiveKata

--- src/main/java/com/org/ita/kata/implementation/TetianaFilatova/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/TetianaFilatova/FiveImpl.java
@@ -7,22 +7,59 @@ import java.math.BigInteger;
 public class FiveImpl implements Five {
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long bonus = 0;
+        for (long i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                if (i - bonus == g) {
+                    return new long[]{bonus, i};
+                }
+                bonus = i;
+            }
+        }
+        return null;
+    }
+
+    private static boolean isPrime(long k) {
+        for (int i = 2; i < (k / 2); i++) {
+            if (k % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public int zeros(int n) {
-        return 0;
+
+        int zeroes = 0;
+        int number = 5;
+        while (number < n) {
+            zeroes += (int) (n / number);
+            number *= 5;
+        }
+        return zeroes;
     }
 
     @Override
     public BigInteger perimeter(BigInteger n) {
-        return null;
+
+        BigInteger X = BigInteger.valueOf(0);
+        BigInteger Y = BigInteger.valueOf(1);
+        BigInteger Z = BigInteger.valueOf(1);
+        BigInteger sum = BigInteger.valueOf(0);
+        for (int i = 0; i <= n.intValue(); i++) {
+            X = Y;
+            Y = Z;
+            Z = X.add(Y);
+            sum = sum.add(X);
+        }
+        return sum.multiply(BigInteger.valueOf(4));
     }
 
     @Override
     public double solve(double m) {
-        return 0;
+        double b = -2 * m - 1;
+        return (-b - Math.sqrt(Math.pow(b, 2) - 4 * m * m)) / (2 * m);
     }
 
     @Override
