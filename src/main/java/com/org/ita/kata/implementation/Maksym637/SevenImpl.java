dd3297c80c2ee14e8f30b2a0f40c90a64da2c9b3
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2023-01-31 19:04:12 +0200

    Adding solutions to kata-7 fixed

--- src/main/java/com/org/ita/kata/implementation/Maksym637/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/Maksym637/SevenImpl.java
@@ -5,11 +5,27 @@ import com.org.ita.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0, count = arr.length + 1;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        long resultOfCounting = (long) Math.ceil(navg * count - sum);
+        if (resultOfCounting <= 0) {
+            throw new IllegalArgumentException();
+        }
+        return resultOfCounting;
     }
 
     @Override
     public String seriesSum(int n) {
-        return null;
+        int counter = 1;
+        double init = 4;
+        double result = 1;
+        while (counter < n) {
+            result += 1 / init;
+            init += 3;
+            counter += 1;
+        }
+        return String.format("%.2f", result);
     }
 }
